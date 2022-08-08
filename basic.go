acc6851a677638e6ba3b1e848df5d4365a2982fa
qw20012 <barret.qin@gmail.com>
2022-08-08 11:01:36 +0800

    changes for v0.1.0

--- basic.go
+++ basic.go
@@ -5,50 +5,21 @@
 // Basic utility functions.
 package basic
 
-import (
-	"reflect"
-)
+import "github.com/qw20012/go-basic/ref"
 
-// Identify whether the any type is nil.
-func IsNil(value any) bool {
-	if value == nil {
-		return true
-	}
-
-	switch reflect.TypeOf(value).Kind() {
-	case reflect.Ptr, reflect.Map, reflect.Chan, reflect.Slice:
-		return reflect.ValueOf(value).IsNil()
-	}
-
-	return false
+// Get the exact value of any.
+func FromAny[T any](value any) T {
+	return value.(T)
 }
 
 // Make sure any type is created. Create by reflect if it is not there.
 func NewIfEmpty[T any](value T) T {
-	if !IsNil(value) {
+	if !ref.IsNil(value) {
 		return value
 	}
 
-	mapReflect := DynamicNew(value)
-	creatd := mapReflect.Interface().(T)
+	newValue := ref.ReflectNew(value)
+	//creatd := newValue.Interface().(T)
+	creatd := ref.GetValue[T](newValue)
 	return creatd
-}
-
-// Todo: Would complete this function in future.
-func DynamicNew(value any) reflect.Value {
-	ty := reflect.TypeOf(value)
-	switch ty.Kind() {
-	case reflect.Interface:
-		return reflect.MakeMap(ty).Elem()
-	case reflect.Ptr:
-		return reflect.New(ty.Elem()).Elem()
-	case reflect.Map:
-		return reflect.MakeMap(ty)
-	case reflect.Struct:
-		return reflect.New(ty.Elem()).Elem()
-	case reflect.Slice:
-		return reflect.MakeSlice(ty, 0, 0)
-	}
-
-	return reflect.ValueOf(value)
 }
--- basic_test.go
+++ basic_test.go
@@ -4,45 +4,26 @@ import (
 	"testing"
 )
 
-func TestIsNil(t *testing.T) {
-	// Test Array
-	var emptyArray [1]int
-	if IsNil(emptyArray) {
-		t.Fatalf("GetOrCreate with emtpy slice failed")
-	}
-
-	// Test Struct
-	type Geek struct {
-		A int `tag1:"First Tag" tag2:"Second Tag"`
-		B string
-	}
-	var p Geek
-	if IsNil(p) {
-		t.Fatalf("GetOrCreate with emtpy struct pointer ")
-	}
-
-}
-
-func TestGetOrCreate(t *testing.T) {
+func TestNewIfEmpty(t *testing.T) {
 	// Test map
 	var emptyAnyMap map[string]any
 	fromEmptyAnyMap := NewIfEmpty(emptyAnyMap)
 	if fromEmptyAnyMap == nil {
-		t.Fatalf("GetOrCreate with emtpy map failed")
+		t.Fatalf("NewIfEmpty with emtpy map failed")
 	}
 	fromEmptyAnyMap["key"] = 1
 
 	var emptyIntMap map[string]int
 	fromEmptyIntMap := NewIfEmpty(emptyIntMap)
 	if fromEmptyIntMap == nil {
-		t.Fatalf("GetOrCreate with emtpy int map failed")
+		t.Fatalf("NewIfEmpty with emtpy int map failed")
 	}
 	fromEmptyIntMap["key"] = 1
 
 	notEmptyMap := make(map[string]int)
 	fromNotEmptyMap := NewIfEmpty(notEmptyMap)
 	if fromNotEmptyMap == nil {
-		t.Fatalf("GetOrCreate with not emtpy map failed")
+		t.Fatalf("NewIfEmpty with not emtpy map failed")
 	}
 	fromNotEmptyMap["key"] = 1
 
@@ -50,18 +31,18 @@ func TestGetOrCreate(t *testing.T) {
 	var emptySlice []int
 	fromEmptySlice := NewIfEmpty(emptySlice)
 	if fromEmptySlice == nil {
-		t.Fatalf("GetOrCreate with emtpy slice failed")
+		t.Fatalf("NewIfEmpty with emtpy slice failed")
 	}
 	if len(append(fromEmptySlice, 1)) != 1 {
-		t.Fatalf("GetOrCreate with emtpy slice failed")
+		t.Fatalf("NewIfEmpty with emtpy slice failed")
 	}
 
 	notEmptySlice := []int{1}
 	fromNotEmptySlice := NewIfEmpty(notEmptySlice)
 	if fromNotEmptySlice == nil {
-		t.Fatalf("GetOrCreate with emtpy slice failed")
+		t.Fatalf("NewIfEmpty with emtpy slice failed")
 	}
 	if len(append(fromNotEmptySlice, 1)) != 2 {
-		t.Fatalf("GetOrCreate with emtpy slice failed")
+		t.Fatalf("NewIfEmpty with emtpy slice failed")
 	}
 }
